3916788a4ed535341e952e278407196e155d178b
jtran1999 <jtan@gmail.com>
2019-09-14 15:03:31 -0700

    additions to banip.sh script for better output readability

--- banip.sh
+++ banip.sh
@@ -4,6 +4,14 @@ echo The ip to ban is $ipaddr
 echo $1
 sed -i "/^iptables -A INPUT -j LOG/a iptables -A INPUT -s $ipaddr/32 -p tcp -j DROP" addrules
 ./addrules
+echo
+echo
+echo
+echo this is in the active firewall rules
 iptables -L -n |grep $ipaddr
+echo
+echo
+echo
+echo this is in the addrules file
 grep $ipaddr addrules
 echo done
