42a16dd2ac9f0a99883047fa715e459c326d4419
jtran1999 <jtan@gmail.com>
2019-09-15 02:33:33 -0700

    update banip script for full condition check

--- banip.sh
+++ banip.sh
@@ -1,17 +1,20 @@
 #!/bin/bash
-read -p 'IP to ban: ' ipaddr
-echo The ip to ban is $ipaddr
-echo $1
-sed -i "/^iptables -A INPUT -j LOG/a iptables -A INPUT -s $ipaddr/32 -p tcp -j DROP" addrules
-./addrules
+read -p 'IP to look for: ' ip
 echo
+echo var is $ip
+if grep -Fq $ip  addrules
+then
+    echo Ip already exist in addrules
+    iptables -L -n |grep $ip
+    # code if found
+else
+    echo does not exist, it needs to be added to addrules
+    echo adding ip to addrules and processing updates
+    sed -i "/^iptables -A INPUT -j LOG/a iptables -A INPUT -s $ipaddr/32 -p tcp -j DROP" addrules
+    ./addrules
+    iptables -L -n |grep $ip 
+    # code if not found
+fi
 echo
-echo
-echo this is in the active firewall rules
-iptables -L -n |grep $ipaddr
-echo
-echo
-echo
-echo this is in the addrules file
-grep $ipaddr addrules
 echo done
+
